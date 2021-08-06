c2b3b5bd37392c64f5adadce23d816299f1b79ca
Tatsiana Yeliseyeva <roimze@mail.ru>
2021-08-06 21:03:46 +0300

    This is the task with a some operations with array: finding of maximum and minimum from the elements of the massive; outputting even and odd elements; summa of the elements

---
+++ src/OperationWhitArray.java
@@ -0,0 +1,52 @@
+import java.util.Arrays;
+
+public class OperationWhitArray {
+    public static void main(String[] args) {
+        int max = 0;
+        int[][] array = {{1, 2, 3}, {4, 5, 6}};
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if (max < array[i][j]) {
+                    max = array[i][j];
+                }
+            }
+        }
+        System.out.println(max);
+        int min = array[0][0];
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if (min > array[i][j]) {
+                    min = array[i][j];
+                }
+            }
+        }
+        System.out.println(min);
+        System.out.print("Чётные элементы массива: ");
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if (array[i][j] % 2 == 0) {
+                    System.out.print(array[i][j]);
+
+                }
+            }
+        }
+        System.out.println(" ");
+        System.out.print("Нечётные элементы массива: ");
+                for (int i = 0; i < array.length; i++) {
+                    for (int j = 0; j < array[i].length; j++) {
+                if (array[i][j]%2 != 0) {
+                    System.out.print(array[i][j]);
+                }
+            }
+        }
+        System.out.println(" ");
+                int summa = 0;
+                for (int i = 0; i < array.length; i++) {
+                    for (int j = 0; j < array[i].length; j++) {
+                        summa = summa + array[i][j];
+
+                    }
+                    }
+        System.out.println("Сумма элементов массива: " + summa);
+                }
+}

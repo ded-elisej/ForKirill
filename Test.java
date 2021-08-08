16501d87c11f828310ca612b781123069b086525
ded-elisej <77746790+ded-elisej@users.noreply.github.com>
2021-08-08 18:59:28 +0200

    Add files via upload
    The abstract class Figures, which describes some geometric figures, like triangle, rectangle, quadrate

---
+++ Test.java
@@ -0,0 +1,10 @@
+public class Test {
+    public static void main(String[] args) {
+        Triangle triangle1 = new Triangle(10, 10, 60);
+        triangle1.square();
+        triangle1.heigh();
+        triangle1.perimetre();
+        triangle1.inform();
+        triangle1.message();
+    }
+}
